ea3a88493bb7b34d1caa6ca289c6c684f95808a0
Michael Hemming <m.hemming@unsw.edu.au>
2020-12-09 17:40:48 +1100

    Added KPSS test and innovative trend analysis example, changed to use surface data instead of 19m for now

--- Trends_Port_Hacking.py
+++ Trends_Port_Hacking.py
@@ -6,9 +6,11 @@
 # %% -----------------------------------------------------------------------------------------------
 # Import modules
 
+import os
+os.chdir('C:\\Users\\mphem\\Documents\\Work\\UNSW\\Trends\\Trend_Analysis_Scripts')
 # general functions
 import xarray as xr
-# import matplotlib as mat
+import matplotlib as mat
 import matplotlib.pyplot as plt
 import datetime as dt
 import numpy as np
@@ -24,6 +26,10 @@ from sklearn import linear_model
 import scipy as sp
 # Import functions from script
 import Trends_functions as TF
+# KPSS test
+from statsmodels.tsa.stattools import kpss
+# 1D interpolation
+from scipy.interpolate import interp1d
 
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -47,6 +53,7 @@ PDO_IND_t = PDO_IND_t[c]
 SAM = pd.read_csv(main_path + 'Data\\SAM.csv',index_col=0) 
 # need to add code here to sort out SAM
 
+del c, main_path
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -55,13 +62,13 @@ SAM = pd.read_csv(main_path + 'Data\\SAM.csv',index_col=0)
 # Select data at specific depths
 
 # 17-21 m depth
-c19 = [(NRSPHB_agg.DEPTH >= 17) & (NRSPHB_agg.DEPTH <= 21)]
-D19 = np.array(NRSPHB_agg.DEPTH)
-D19 = D19[c19]
-t19 = np.array(NRSPHB_agg.TIME)
-t19 = t19[c19]
-T19 = np.array(NRSPHB_agg.TEMP)
-T19 = T19[c19]
+csurf = [(NRSPHB_agg.DEPTH >= 0) & (NRSPHB_agg.DEPTH <= 2)]
+Dsurf = np.array(NRSPHB_agg.DEPTH)
+Dsurf = Dsurf[csurf]
+tsurf = np.array(NRSPHB_agg.TIME)
+tsurf = tsurf[csurf]
+Tsurf = np.array(NRSPHB_agg.TEMP)
+Tsurf = Tsurf[csurf]
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -69,7 +76,8 @@ T19 = T19[c19]
 # %% -----------------------------------------------------------------------------------------------
 # Bin the data
 
-tbin,Tbin = TF.bin_daily(1953,2020,t19,T19)
+# This is done to get a regular time grid with daily resolution
+tbin,Tbin = TF.bin_daily(1953,2020,tsurf,Tsurf)
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -77,28 +85,10 @@ tbin,Tbin = TF.bin_daily(1953,2020,t19,T19)
 # %% -----------------------------------------------------------------------------------------------
 # De-season data
 
-# get climatology grid
-clim_grid = range(0,365)
-# convert times into year days
-tbin_doy = []
-for n in range(len(tbin)):
-    t = time_grid[n]
-    tbin_doy.append(int(t.strftime('%j')))
-
-clim19 = NRSPHB_clim.TEMP_AVE[:,2] # climatology at 20m
-
-
-Tbin_deseason = [None] * len(tbin_doy)
-for n in range(len(tbin_doy)):
-    if tbin_doy[n]-1 < 365:
-        Tbin_deseason[n] = Tbin[n] - clim19[tbin_doy[n]-1]
-    else:
-        Tbin_deseason[n] = np.nan
-
-# plot climatology
-# plt.scatter(tbin_doy,Tbin)
-# plt.plot(clim_grid,clim19,color='r')
-# plt.show()
+# select climatology at similar depth
+climsurf = NRSPHB_clim.TEMP_AVE[:,0] # climatology at 20m
+# get de-seasoned temperatures
+Tbin_deseason = TF.deseason(tbin,Tbin,climsurf)
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -107,60 +97,8 @@ for n in range(len(tbin_doy)):
 # %% -----------------------------------------------------------------------------------------------
 # Get annual averages
 
-# Create time grid
-base = dt.datetime(1953, 6, 1)
-time_grid_ann = np.array([base + dt.timedelta(days=i*365.25) for i in range(0,68)])
-base = dt.datetime(1953, 1, 1)
-time_grid_ann_lower = np.array([base + dt.timedelta(days=i*365.25) for i in range(0,68)])
-base = dt.datetime(1954, 1, 1)
-time_grid_ann_upper = np.array([base + dt.timedelta(days=i*365.25) for i in range(0,68)])
-
-
-t_grid_ann = []; # convert to datetime
-t_grid_ann_lower = []; # convert to datetime
-t_grid_ann_upper = []; # convert to datetime
-
-for n in range(len(time_grid_ann)):
-    t_grid_ann.append(np.datetime64(time_grid_ann[n]))
-    t_grid_ann_lower.append(np.datetime64(time_grid_ann_lower[n]))
-    t_grid_ann_upper.append(np.datetime64(time_grid_ann_upper[n]))
-
-# convert times into year and months
-# https://strftime.org/
-tbin_y = []
-tbin_m = []
-for n in range(len(tbin)):
-    t = time_grid[n]
-    tbin_y.append(int(t.strftime('%Y')))
-    tbin_m.append(int(t.strftime('%m')))
-
-un_yrs = np.unique(tbin_y)
-
-
-# binning
-Tbin_ann = []
-tbin_ann = []
-for n_bin in range(len(un_yrs)):
-        c = tbin_y == un_yrs[n_bin]
-        T_in_bin = []
-        t_in_bin = []
-        m_in_bin = []
-        yr_in_bin = []
-        for n in range(len(c)):
-            if c[n] and np.isnan(Tbin_deseason[n]) == False:
-                T_in_bin.append(Tbin_deseason[n])
-                t_in_bin.append(tbin[n])
-                m_in_bin.append(tbin_m[n])
-                yr_in_bin.append(tbin_y[n])
-                
-        if len(np.unique(m_in_bin)) > 6:
-            Tbin_ann.append(np.nanmean(T_in_bin))
-            tbin_ann.append(dt.datetime(int(un_yrs[n_bin]),6,1))    
-        else:
-            Tbin_ann.append(np.nan)
-            tbin_ann.append(dt.datetime(int(un_yrs[n_bin]),6,1))     
+tbin_ann,Tbin_ann = TF.bin_annually(1953,2020,tbin,Tbin_deseason)
    
-
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -183,20 +121,104 @@ PDO_result = sp.stats.linregress(PDO_IND_reg,Tbin_reg); # no real relationship
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # %% -----------------------------------------------------------------------------------------------
+# KPSS test to check for stationarity
+# If the result = 'Not stationary', a deterministc trend / linear regression is not suitable
+
+TF.kpss_test(Tbin_deseason)
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+# %% -----------------------------------------------------------------------------------------------
+# Innovative trend analysis
+
+# separate the period into two
+T1 = np.array(np.sort(Tbin_ann[0:33]))
+T2 = np.array(np.sort(Tbin_ann[33:-2]))
+
+T1_indices = np.argsort(Tbin_ann[0:33]) 
+tb = np.array(tbin_ann[0:33])
+T1_t = tb[T1_indices]
+T2_indices = np.argsort(Tbin_ann[33:-2]) 
+tb = np.array(tbin_ann[33:-2])
+T2_t = tb[T2_indices]
+
+# get 'low' mean slope
+cl = [(T1 < -0.88) & (T2 < -0.5)]
+low_stats = sp.stats.linregress(T1[cl],T2[cl])
+
+# get 'medium' mean slope
+cm = [(T1 > -0.88) & (T1 < -0.25) & (T2 > -0.5) & (T2 < 0.45)]
+med_stats = sp.stats.linregress(T1[cm],T2[cm])
+
+# get 'high' mean slope
+ch = [(T1 >= -0.25) & (T2 >= 0.45)]
+high_stats = sp.stats.linregress(T1[ch],T2[ch])
+
+# 1:1 line (no trend line)
+line = np.arange(start=-1.5, stop=2.5, step=0.1)
+# Determine trends
+#------------------------
+ll = T2[cl]
+low_res = []
+for n in range(len(ll)):
+    low_res.append(np.abs(ll[n] - (low_stats.slope*ll[n]+low_stats.intercept)))
+#------------------------
+lm = T2[cm]
+med_res = []
+for n in range(len(lm)):
+    med_res.append(np.abs(lm[n] - (med_stats.slope*lm[n]+med_stats.intercept)))
+#------------------------
+lh = T2[ch]
+high_res = []
+for n in range(len(lh)):
+    high_res.append(np.abs(lh[n] - (high_stats.slope*lh[n]+high_stats.intercept)))
+trend_mean_slope = np.mean([np.mean(high_res),np.mean(med_res),np.mean(low_res)])
+# trend_century = ((1-(len(T1)+len(T2))/100) * trend_mean_slope) + trend_mean_slope 
+trend_century = (trend_mean_slope/66)*100
+
+a= plt.figure()
+axes = a.add_axes([0.1,0.1,0.8,0.8])
+axes.scatter(T1,T2)
+axes.scatter(T1[cl],T2[cl])
+axes.scatter(T1[cm],T2[cm])
+axes.plot(T1[cl],low_stats.slope*T1[cl]+low_stats.intercept,'k')
+axes.plot(T1[cm],med_stats.slope*T1[cm]+med_stats.intercept,'k')
+axes.plot(T1[ch],high_stats.slope*T1[ch]+high_stats.intercept,'k')
+axes.set_xlim([-1.5, 2.5])
+axes.set_ylim([-1.5, 2.5])
+axes.set_xlabel('1953 - 1985')
+axes.set_ylabel('1986 - 2018')
+axes.plot(line,line,'k')
+plt.show()
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+# %% -----------------------------------------------------------------------------------------------
 # estimate trend with sensitivity testing
 # changing bin sizes, changing depth, changing start and end date
-
-
 
 
 # Mann kendall tests
 
 mk_result = mk.original_test(Tbin_ann)
+mk_result_wang = mk.pre_whitening_modification_test(Tbin_ann)
 mk_trend = range(0,68)*mk_result.slope + mk_result.intercept; # Theil-Sen slope
+mk_trend_wang = range(0,68)*mk_result_wang.slope + mk_result_wang.intercept; # Theil-Sen slope
 
 plt.scatter(tbin_ann,Tbin_ann)
 plt.plot(tbin_ann,mk_trend,'r')
+plt.plot(tbin_ann,mk_trend_wang,'g')
 plt.show()
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 
 # %% -----------------------------------------------------------------------------------------------
 # Pettitt tests
