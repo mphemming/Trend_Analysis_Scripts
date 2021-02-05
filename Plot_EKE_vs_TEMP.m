6b768cdf007afd9dc5d82c74ea4484b305bfcc54
Michael Hemming <m.hemming@unsw.edu.au>
2021-02-05 17:50:11 +1100

    Modified IMFs and created plot

--- Plot_EKE_vs_TEMP.m
+++ Plot_EKE_vs_TEMP.m
@@ -56,8 +56,8 @@ end
 figure('units','normalized','position',[0 0.1 .6 .75]);
 
 
-MAI_EKE = NRSMAI_EKE.EEMD_imfs(7,:) + NRSMAI_EKE.EEMD_imfs(8,:);
-MAI_tr = NRSMAI_trends_server.EEMD_trend_EAC{1};
+MAI_EKE = NRSMAI_EKE.EEMD_imfs(7,:);
+MAI_tr = NRSMAI_trends_server.EEMD_imfs.IMF_1(8,:);
 MAI_tr_t = NRSMAI_trends.EEMD_t_conv(1).t;
 
 for n = 1:numel(NRSMAI_EKE.EEMD_t_conv(1).t)
@@ -70,8 +70,8 @@ end
 bin.EKE_MAI = bin.EKE_MAI-bin.EKE_MAI(1);
 bin.TEMP_MAI = bin.TEMP_MAI - bin.TEMP_MAI(1);
 
-PHB_EKE = NRSPHB_EKE.EEMD_imfs(7,:) + NRSPHB_EKE.EEMD_imfs(8,:);
-PHB_tr = NRSPHB_trends_server.EEMD_trend_EAC{1};
+PHB_EKE = NRSPHB_EKE.EEMD_imfs(7,:) ;
+PHB_tr = NRSPHB_trends_server.EEMD_imfs.IMF_1(7,:);
 PHB_tr_t = NRSPHB_trends.EEMD_t_conv(1).t;
 
 for n = 1:numel(NRSPHB_EKE.EEMD_t_conv(1).t)
@@ -84,12 +84,34 @@ end
 bin.EKE_PHB = bin.EKE_PHB-bin.EKE_PHB(1);
 bin.TEMP_PHB = bin.TEMP_PHB - bin.TEMP_PHB(1);
 
+bin.EKE_PHB = (bin.EKE_PHB-nanmin(bin.EKE_PHB))/(nanmax(bin.EKE_PHB)-nanmin(bin.EKE_PHB));
+bin.EKE_MAI = (bin.EKE_MAI-nanmin(bin.EKE_MAI))/(nanmax(bin.EKE_MAI)-nanmin(bin.EKE_MAI));
+bin.TEMP_PHB = (bin.TEMP_PHB-nanmin(bin.TEMP_PHB))/(nanmax(bin.TEMP_PHB)-nanmin(bin.TEMP_PHB));
+bin.TEMP_MAI = (bin.TEMP_MAI-nanmin(bin.TEMP_MAI))/(nanmax(bin.TEMP_MAI)-nanmin(bin.TEMP_MAI));
+
+figure('units','normalized','position',[0 0.1 .6 .75]);
+
 scatter(bin.EKE_PHB,bin.TEMP_PHB);
 hold on
 scatter(bin.EKE_MAI,bin.TEMP_MAI)
 
+leg = legend('NRSPHB IMF_{A} 1993-2020','NRSMAI IMF_{A} 1993-2020');
+set(leg,'Box','Off','Location','NorthWest');
+% ylabel('NRSPHB Trend_{B+A} [^\circC]');
+% ylabel('NRSMAI Trend_{B+A} [^\circC]');
+ylim([0 1])
+set(gca,'FontSize',18,'LineWidth',2,'Box','On');
+grid on
+ylabel('Normalised Temperature');
+xlabel('Normalised Eddy Kinetic Energy');
 
+% fit
+c = isfinite(bin.TEMP_PHB) & isfinite(bin.EKE_PHB);
+[f_PHB,g_PHB,o_PHB] = fit(bin.EKE_PHB(c)',bin.TEMP_PHB(c)','poly1');
+c = isfinite(bin.TEMP_MAI) & isfinite(bin.EKE_MAI);
+[f_MAI,g_MAI,o_MAI] = fit(bin.EKE_MAI(c)',bin.TEMP_MAI(c)','poly1');
 
+print(gcf, '-dpng','-r400', [options.plot_dir,'EKE_comparison'])
 
 
 
