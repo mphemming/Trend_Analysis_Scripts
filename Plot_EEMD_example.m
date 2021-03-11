a32039ac55ccb7f55d73f1b33e1ee8617ca55c56
Michael Hemming <m.hemming@unsw.edu.au>
2021-03-11 16:59:43 +1100

    loading temporary data for now, some changes but will wait until rest of the data is available before changinf

--- Plot_EEMD_example.m
+++ Plot_EEMD_example.m
@@ -7,22 +7,22 @@ options.plot_dir = 'C:\Users\mphem\Documents\Work\UNSW\Trends\Plots\';
 % NRSPHB
 NRSPHB_data = load([options.data_dir,'NRSPHB_data']);
 NRSPHB_data_server = load([options.data_dir,'NRSPHB_data_server']);
-NRSPHB_trends = load([options.data_dir,'NRSPHB_trends']);
+NRSPHB_trends = load([options.data_dir,'NRSPHB_trends_for_figure1']);
 NRSPHB_trends_server = load([options.data_dir,'NRSPHB_trends_server']);
 
 %% Sort out time
 % NRSPHB
 % time
 nt = size(NRSPHB_data.t,2);
-for nn = 1:9
+for nn = 1
     for t = 1:nt
         a = squeeze(vertcat(NRSPHB_data.t(nn,t,:)))';
         NRSPHB_data.t_conv(nn).t(t) = datenum(convertCharsToStrings(a));
     end
 end
 % EEMD time
-for nn = 1:9
-    a = NRSPHB_trends.EEMD_t{nn};
+for nn = 1
+    a = squeeze(NRSPHB_trends.EEMD_t(nn,:,:));
     for t = 1:size(a,1)
         b = a(t,:);
         NRSPHB_trends.EEMD_t_conv(nn).t(t) = datenum(convertCharsToStrings(b));
@@ -37,13 +37,15 @@ axes('Parent',gcf,...
     'Position',[0.0646701388888889 0.11 0.52662037037037 0.815]);
 
 hold on 
-std_T = nanstd(NRSPHB_trends.EEMD_T{1});
-tr = NRSPHB_trends.EEMD_trend{1} / std_T;
-tr1 = tr(1)
+std_T = nanstd(NRSPHB_trends.EEMD_T(1,:));
+% tr = NRSPHB_trends.EEMD_trend(1,:) / std_T;
+tr = NRSPHB_trends.EEMD_trend(1,:)
+tr1 = tr(1);
 tr = tr-tr1;
-tr_EAC = NRSPHB_trends.EEMD_trend_EAC{1} / std_T;
+% tr_EAC = NRSPHB_trends.EEMD_trend_EAC(1,:) / std_T;
+tr_EAC = NRSPHB_trends.EEMD_trend_EAC(1,:);
 tr_EAC = tr_EAC-tr_EAC(1);
-p1 = plot(NRSPHB_trends.EEMD_t_conv(1).t,(NRSPHB_trends.EEMD_T{1}-tr1)/std_T,'LineWidth',2)
+p1 = plot(NRSPHB_trends.EEMD_t_conv(1).t,(NRSPHB_trends.EEMD_T(1,:)-tr1)/std_T,'LineWidth',2)
 p2 = plot(NRSPHB_trends.EEMD_t_conv(1).t,tr,'LineWidth',2,'Color','r')
 p3 = plot(NRSPHB_trends.EEMD_t_conv(1).t,tr_EAC,'LineWidth',2,'Color','r')
 % determine where significant
@@ -71,21 +73,21 @@ p5 = plot(NRSPHB_trends.EEMD_t_conv(1).t(sig_EAC == 1),tr_EAC(sig_EAC == 1)-tr(1
 set(p3b,'FaceColor','r','FaceAlpha',0.2); set(p3a,'LineStyle','None');
 set(p4b,'FaceColor','r','FaceAlpha',0.3); set(p4a,'LineStyle','None');
 
-set(gca,'LineWidth',2,'Box','On','FontSize',18,'YLim',[-3 4],'XLim',[datenum(1950,01,01) datenum(2021,01,01)],...
+set(gca,'LineWidth',2,'Box','On','FontSize',18,'YLim',[-3.5 5],'XLim',[datenum(1950,01,01) datenum(2021,01,01)],...
     'XTick',[datenum(1950,01,01) datenum(1960,01,01) datenum(1970,01,01) datenum(1980,01,01) datenum(1990,01,01) ...
     datenum(2000,01,01) datenum(2010,01,01) datenum(2020,01,01)],'XTickLabels',[{'1950'} {'1960'} {'1970'} {'1980'} {'1990'} {'2000'} {'2010'} {'2020'}])
 
 ylabel('Temperature Anomaly [^\circC]');
 
 leg = legend([p1 p4 p5 p3b p4b],'De-seasoned temperatures', ...
-    'Significant EEMD Trend_{B}','Significant EEMD Trend_{B+A}','Boundary of insignificance Trend_{B+A}','Boundary of insignificance Trend_{B}');
+    'Significant EEMD \itT_{\rmR}','Significant EEMD \itT_{\rmR+IMF}','Boundary of insignificance \itT_{\rmR+IMF}','Boundary of insignificance \itT_{\rmR}');
 set(leg,'Location','NorthWest','Box','Off','Position',[0.101466049382716 0.737275373893183 0.238859958518986 0.146990744076638],...
     'FontSize',14);
 
 axes('Parent',gcf,...
     'Position',[0.620804398148148 0.11 0.284195601851853 0.815]);
 
-p1 = plot(NRSPHB_trends.EEMD_t_conv(1).t,NRSPHB_trends.EEMD_T{1}-tr(1),'LineWidth',2)
+p1 = plot(NRSPHB_trends.EEMD_t_conv(1).t,NRSPHB_trends.EEMD_T(1,:)-tr(1),'LineWidth',2)
 hold on;
 
 imf = NRSPHB_trends.EEMD_imfs.IMF_1;
@@ -132,9 +134,10 @@ annotation(gcf,'textbox',...
     'FontSize',18,...
     'FitBoxToText','off');
 
-leg = legend([p2 p4 p5 p6],'IMFs','IMF_{A}','Trend_{B}','Trend_{B+A}');
+leg = legend([p2 p4 p5 p6],'\itIMFs','\itIMF_{Last}','\itT_{R}','\itT_{R+IMF}');
 set(leg,'Location','SouthWest','Box','Off','FontSize',14);
 
+%%
 print(gcf, '-dpng','-r400', [options.plot_dir,'EEMD_example_NRSPHB_2m'])
 
 
