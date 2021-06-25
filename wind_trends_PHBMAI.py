bc86da81700e11cc2b8d533d0a3256d31346d706
Michael Hemming <m.hemming@unsw.edu.au>
2021-06-25 21:16:47 +1000

    Investigating wind direction

--- wind_trends_PHBMAI.py
+++ wind_trends_PHBMAI.py
@@ -83,12 +83,14 @@ for n in range(len(SYDAIR['Year'])):
     SYDAIR_TIME.append(np.datetime64(date))
     
 SYDAIR_TIME = np.array(SYDAIR_TIME)
-_, _, _, _, SYDAIR_DOY = TF.datevec(SYDAIR_TIME)
+SYDAIR_YR, _, _, _, SYDAIR_DOY = TF.datevec(SYDAIR_TIME)
 
 
 # grab wind data
 SYDAIR_WIND = np.array(SYDAIR['Wind speed measured in km/h'])
 SYDAIR_WIND[SYDAIR_WIND == 0] = np.nan
+SYDAIR_DIR = np.array(SYDAIR['Wind direction measured in degrees'])
+SYDAIR_DIR[SYDAIR_DIR == 0] = np.nan
 # convert to m/s
 SYDAIR_WIND = (SYDAIR_WIND*1000)/3600
 
@@ -129,12 +131,14 @@ for n in range(len(MAI['Year'])):
     MAI_TIME.append(np.datetime64(date))
     
 MAI_TIME = np.array(MAI_TIME)
-_, _, _, _, MAI_DOY = TF.datevec(MAI_TIME)
+MAI_YR, _, _, _, MAI_DOY = TF.datevec(MAI_TIME)
 
 
 # grab wind data
 MAI_WIND = np.array(MAI['Wind speed measured in km/h'])
 MAI_WIND[MAI_WIND == 0] = np.nan
+MAI_DIR = np.array(MAI['Wind direction measured in degrees'])
+MAI_DIR[MAI_DIR == 0] = np.nan
 # convert to m/s
 MAI_WIND = (MAI_WIND*1000)/3600
 
@@ -145,6 +149,7 @@ MAI_WIND = (MAI_WIND*1000)/3600
 # Sydney Airport
 # calculate simple climatology
 clim = TF.calc_clim_monthly(SYDAIR_TIME,SYDAIR_WIND)  
+clim_DIR = TF.calc_clim_monthly(SYDAIR_TIME,SYDAIR_DIR)  
 # get de-seasoned anomalies
 
 SYDAIR_TIME_m_deseason = []
@@ -153,11 +158,16 @@ SYDAIR_WIND_m_deseason = []
 SYDAIR_TIME_m,WW = TF.bin_monthly(1953,2021,SYDAIR_TIME,np.float64(SYDAIR_WIND))
 SYDAIR_WIND_m_deseason,SYDAIR_WIND_m,_,SYDAIR_WIND_m_no_fill = TF.fill_gaps(
                                     SYDAIR_TIME_m,WW,np.squeeze(clim),30*12)
+# wind direction
+_,WD = TF.bin_monthly(1953,2021,SYDAIR_TIME,np.float64(SYDAIR_DIR))
+SYDAIR_DIR_m_deseason,SYDAIR_DIR_m,_,SYDAIR_DIR_m_no_fill = TF.fill_gaps(
+                                    SYDAIR_TIME_m,WD,np.squeeze(clim_DIR),30*12)
 
 #########################################################################
 # MAI
 # calculate simple climatology
 clim = TF.calc_clim_monthly(MAI_TIME,MAI_WIND)  
+clim_DIR = TF.calc_clim_monthly(MAI_TIME,MAI_DIR) 
 # get de-seasoned anomalies
 
 MAI_TIME_m_deseason = []
@@ -166,6 +176,45 @@ MAI_WIND_m_deseason = []
 MAI_TIME_m,WW = TF.bin_monthly(1953,2021,MAI_TIME,np.float64(MAI_WIND))
 MAI_WIND_m_deseason,MAI_WIND_m,_,MAI_WIND_m_no_fill = TF.fill_gaps(
                                     MAI_TIME_m,WW,np.squeeze(clim),30*12)
+
+# wind direction
+_,WD = TF.bin_monthly(1953,2021,MAI_TIME,np.float64(MAI_DIR))
+MAI_DIR_m_deseason,MAI_DIR_m,_,MAI_DIR_m_no_fill = TF.fill_gaps(
+                                    MAI_TIME_m,WD,np.squeeze(clim_DIR),30*12)
+
+
+# %% -------------------------------------------------------
+# Calculate index when northerly or southerly wind
+# when =0/360 wind coming from the wind
+# when = 180 wind coming from the south
+
+#########################################################################
+# Sydney Airport
+syd_northerly = []
+syd_southerly = []
+for n in range(len(SYDAIR_DIR)):
+    if SYDAIR_DIR[n] > 140 or SYDAIR_DIR[n] < 220:
+        syd_southerly.append(1)
+    else:
+        syd_southerly.append(0)
+    if SYDAIR_DIR[n] > 320 or SYDAIR_DIR[n] < 40:
+        syd_northerly.append(1)
+    else:
+        syd_northerly.append(0)        
+syd_northerly = np.array(syd_northerly)
+syd_southerly = np.array(syd_southerly)
+# get annual sums (proportion)
+un_yrs = np.unique(SYDAIR_YR)
+syd_northerly_prop = []
+syd_southerly_prop = []
+for n in range(len(un_yrs)):
+    c = SYDAIR_YR == un_yrs[n]
+    n_data = np.sum(c)
+    syd_northerly_prop.append(np.sum(syd_northerly[c])/n_data*100)
+    syd_southerly_prop.append(np.sum(syd_southerly[c])/n_data*100)
+syd_northerly_prop = np.array(syd_northerly_prop)            
+syd_southerly_prop = np.array(syd_southerly_prop)    
+    
 
 
 # %% -------------------------------------------------------
